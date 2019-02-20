d43b0b10df9b857af9c02423fff6d42c1863ad6b
Aditya.J <Aditya.J@IDVT-03-56>
2019-02-20 16:23:48 +0530

    all changes

---
+++ SendMail/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SendMail.Startup))]
+namespace SendMail
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
